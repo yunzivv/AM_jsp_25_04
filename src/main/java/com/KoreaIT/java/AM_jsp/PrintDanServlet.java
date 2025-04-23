b881ec8b784db8532ff9db54cdaa812150c7405b
yunzivv <yunzivv@gmail.com>
2025-04-23 15:23:39 +0900

    getParameter ?string=value

--- src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
@@ -15,8 +15,8 @@ public class PrintDanServlet extends HttpServlet {
 		
 		response.setContentType("text/html;charset=UTF-8");
 		
-		response.getWriter().append("== 8단 ==<br>");
-		int dan = 8;
+		int dan = Integer.parseInt(request.getParameter("dan"));
+		response.getWriter().append(String.format("== %d단 ==<br>", dan));
 		for(int i = 1; i < 10; i++) {
 			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));			
 		}
