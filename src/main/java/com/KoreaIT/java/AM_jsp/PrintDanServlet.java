cc7887d7ff4dff89fab2a8807307bd6275940699
yunzivv <yunzivv@gmail.com>
2025-04-23 16:12:59 +0900

    html 섞어서 글자색 변경하기

--- src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
@@ -1,11 +1,13 @@
 package com.KoreaIT.java.AM_jsp;
 
+
+import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @WebServlet("/PrintDan")
@@ -17,17 +19,28 @@ public class PrintDanServlet extends HttpServlet {
 		
 		String dan = request.getParameter("dan");
 		String limit = request.getParameter("limit");
+		String color = request.getParameter("color");
 		
 		if(dan == null) dan = "1";
 		if(limit == null) limit = "1";
+		if(color == null) color = "black";
 		
 		int ddan = Integer.parseInt(dan);
 		int dlimit = Integer.parseInt(limit);
 		
-		response.getWriter().append(String.format("== %d단 ==<br>", ddan));
+		PrintWriter out = response.getWriter();
+		out.print("<html>");
+		out.print("<head> </head>"); 
+		out.print("<body style='color:" + color + "'>");
+		
+		out.append(String.format("== %d단 ==<br>", ddan));
 		for(int i = 1; i <= dlimit; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", ddan, i, ddan * i));			
+			out.append(String.format("%d * %d = %d<br>", ddan, i, ddan * i));			
 		}
+		
+		out.print("</body>");
+		out.print("</html>");
+		
 	}
 
 }
