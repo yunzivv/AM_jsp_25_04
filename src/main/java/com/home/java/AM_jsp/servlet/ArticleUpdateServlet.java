09fab0691e4c3771c2b8b83f435a6edd635a5faa
yunzivv <yunzivv@gmail.com>
2025-04-26 00:48:26 +0900

    insert, update 구조 수정

---
+++ src/main/java/com/home/java/AM_jsp/servlet/ArticleUpdateServlet.java
@@ -0,0 +1,73 @@
+package com.home.java.AM_jsp.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import com.home.java.AM_jsp.util.DBUtil;
+import com.home.java.AM_jsp.util.SecSql;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/doUpdate")
+public class ArticleUpdateServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		// DB 연결
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 x");
+			e.printStackTrace();
+
+		}
+
+		String url = "jdbc:mysql://127.0.0.1:3306/AM_JSP_25_04?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+		String user = "root";
+		String password = "";
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			response.getWriter().append("연결 성공!");
+
+			int id = Integer.parseInt(request.getParameter("id"));
+
+//			String sql = String.format("SELECT * FROM article WHERE id = %d;", id);
+
+			SecSql sql = SecSql.from("SELECT *");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?;", id);
+
+			Map<String, Object> articleRow = DBUtil.selectRow(conn, sql);
+
+			request.setAttribute("articleRow", articleRow);
+
+			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
+
+		} catch (SQLException e) {
+			System.out.println("에러 1 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
